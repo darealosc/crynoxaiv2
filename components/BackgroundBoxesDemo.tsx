be6213c02d598448e9c7131bcfc9603dccb274cb
AarushKamatCodeRVA <aarush.kamat@students.coderva.org>
2025-09-08 17:31:04 +0200

    Added clean code.

--- components/BackgroundBoxesDemo.tsx
+++ components/BackgroundBoxesDemo.tsx
@@ -8,8 +8,6 @@ export function BackgroundBoxesDemo() {
   return (
     <div className="h-800 relative w-350 overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-md">
       <div className="absolute inset-0  bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-
-      <Boxes />
       < FocusCards />
     </div>
   );
--- components/FocusCardsDemo.tsx
+++ components/FocusCardsDemo.tsx
@@ -1,3 +1,5 @@
+
+"use client";
 import { FocusCards } from "@/components/ui/focus-cards";
 
 export function FocusCardsDemo() {
